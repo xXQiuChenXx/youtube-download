bf3dab157008b4da513fe3a2a0a0f2708b3398a9
Qiu Chen <51996408+xXQiuChenXx@users.noreply.github.com>
2024-08-04 18:01:59 +0800

    updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const ytdl = require("@distube/ytdl-core");
 const path = require("path");
 const YouTube = require("youtube-sr").default;
+const prompt = require("prompt-sync")();
 const { process_video } = require("./ffmpeg");
 const { wait } = require("./src/utils");
 const { searchYouTubeVideos } = require("./youtube-search");
@@ -13,51 +14,61 @@ async function start() {
     fs.readFileSync(path.join(__dirname, "./cookies.json"))
   );
   const YTDownload = new Downloader({ cookies });
+
+  const videoURL = prompt("Youtube video link or channel name: ");
+  console.log("Please choose a download format:");
+  console.log("1. MP4");
+  console.log("2. MP3");
+  const choice = prompt("Enter your choice (1, 2, or 3): ");
+  let skipShorts = prompt(
+    "Do you want to download Youtube Shorts? (yes or no)"
+  );
+
+  if (skipShorts === "yes" || skipShorts === "y") skipShorts = true;
+  else skipShorts = false;
+
   // const videoURL = "https://www.youtube.com/watch?v=nlYskBsMbmE";
   // const videoURL = "https://www.youtube.com/watch?v=A5pn4-xOgOw";
-  const videoURL = " NTMusicTrends";
+  // const videoURL = " NTMusicTrends";
   // const videoURL = "https://www.youtube.com/watch?v=nlYskBsMbmE&list=RDnlYskBsMbmE&start_radio=1";
   // const videoURL = "https://www.youtube.com/playlist?list=PLiC7eSfbbxXh2ka0ubC-y1ozKSCtkouUd";
   // const videoURL = "https://www.youtube.com/shorts/7st0X7aAXDQ"
 
   if (videoURL.startsWith("https://www.youtube.com/playlist")) {
     const playlist = await YouTube.getPlaylist(videoURL);
-    return await YTDownload.downloadPlaylist(playlist);
-  }
-
-  if (!videoURL.startsWith("http")) {
+    await YTDownload.downloadPlaylist(playlist);
+  } else if (!videoURL.startsWith("http")) {
     const videos = await searchYouTubeVideos(videoURL);
     for (const video of videos) {
       const videoInfo = await YTDownload.getInfo(
         `https://www.youtube.com/watch?v=${video.id.videoId}`
       );
-      if (videoInfo.videoDetails.lengthSeconds <= 60) continue;
-      await download({
+      if (videoInfo.videoDetails.lengthSeconds <= 60 && skipShorts) continue;
+      await YTDownload.downloadAudio({
         videoURL: `https://www.youtube.com/watch?v=${video.id.videoId}`,
         videoName: video.snippet.title,
       });
       wait(3000);
     }
-    return;
-  }
+  } else {
+    const videoInfo = await YTDownload.getInfo(videoURL);
 
-  const videoInfo = await ytdl.getInfo(videoURL, { agent });
-
-  const { filePath } = await download({
-    videoURL,
-    videoName: videoInfo.videoDetails.title,
-  });
-
-  const chpslength = videoInfo.videoDetails?.chapters.length;
-
-  if (chpslength && chpslength > 2) {
-    // The video have youtube chapters, separate them
-    await process_video({
-      chapters: videoInfo.videoDetails.chapters,
-      filePath,
-      duration: videoInfo.videoDetails.lengthSeconds * 1000,
+    const { filePath } = await YTDownload.downloadAudio({
+      videoURL,
+      videoName: videoInfo.videoDetails.title,
     });
-    await fs.unlinkSync(filePath);
+
+    const chpslength = videoInfo.videoDetails?.chapters.length;
+
+    if (chpslength && chpslength > 2) {
+      // The video have youtube chapters, separate them
+      await process_video({
+        chapters: videoInfo.videoDetails.chapters,
+        filePath,
+        duration: videoInfo.videoDetails.lengthSeconds * 1000,
+      });
+      await fs.unlinkSync(filePath);
+    }
   }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const prompt = require("prompt-sync")();
+
 class MyTest {
   constructor() {
     this.test = "Hello World";
@@ -23,6 +25,18 @@ async function test() {
   const ttest = new MyTest();
 
   await ttest.promisifyFn();
+  const name = prompt("What is your name? ");
+  const age = prompt("What is your age? ");
+  const gender = prompt("What is your gender? ");
+  console.log("Please choose an option:");
+  console.log("1. Say Hello");
+  console.log("2. Say Goodbye");
+  console.log("3. Exit");
+
+  // Prompt the user to make a selection
+  const choice = prompt("Enter your choice (1, 2, or 3): ");
+
+  console.log(`Hello, ${name}, you're ${age} years old, and ${gender} ${choice}!`);
 }
 
 test();
